eab99f92646ef614b90860b8e23723179c340103
cyy <cyy@cyy-MS-7850>
2017-03-02 09:05:06 +0800

    wuya sql change

--- src/main/resources/SQL/test_book.sql
+++ src/main/resources/SQL/test_book.sql
@@ -13,12 +13,3 @@ VALUES
 	(1001, '数据结构', 10),
 	(1002, '设计模式', 10),
 	(1003, '编译原理', 10)
-
--- 创建预约图书表
-CREATE TABLE `appointment` (
-  `book_id` bigint(20) NOT NULL COMMENT '图书ID',
-  `student_id` bigint(20) NOT NULL COMMENT '学号',
-  `appoint_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '预约时间' ,
-  PRIMARY KEY (`book_id`, `student_id`),
-  INDEX `idx_appoint_time` (`appoint_time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='预约图书表'
